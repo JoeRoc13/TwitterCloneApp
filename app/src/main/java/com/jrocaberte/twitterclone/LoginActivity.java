730eabc7963fa5023f31adf434500cf47e665e48
JoeRoc13 <joseph.rocaberte@gmail.com>
2018-04-13 16:33:03 -0400

    Login works

--- app/src/main/java/com/jrocaberte/twitterclone/LoginActivity.java
+++ app/src/main/java/com/jrocaberte/twitterclone/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.jrocaberte.twitterclone;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -14,6 +18,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,12 +43,12 @@ public class LoginActivity extends AppCompatActivity {
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                // Instantiate the RequestQueue.
                 RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
                 //this is the url where you want to send the request
                 //TODO: replace with your own url to send request, as I am using my own localhost for this tutorial
-                String url = "http://10.253.80.182/Twitter_Clone/API/login.php";
+                String url = "http://10.0.0.33/Twitter_Clone/API/login.php";
+                final String username = mUsername.getText().toString();
+                final String password = mPassword.getText().toString();
 
                 // Request a string response from the provided URL.
                 StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
@@ -49,6 +56,41 @@ public class LoginActivity extends AppCompatActivity {
                             @Override
                             public void onResponse(String response) {
                                 // Display the response string.
+//                                SharedPreferences userInfo = getPreferences(Context.MODE_PRIVATE);
+//                                SharedPreferences.Editor edit = userInfo.edit();
+                                try {
+                                    JSONObject jsonObject= new JSONObject(response.toString());
+                                    String success = jsonObject.getString("success");
+                                    if(success.equals("true")) {
+                                        // Get columns from response
+                                        Integer uid = Integer.parseInt(jsonObject.getString("uid"));
+                                        String username = jsonObject.getString("username");
+                                        String email = jsonObject.getString("email");
+                                        String location = jsonObject.getString("location");
+                                        String regis_date = jsonObject.getString("regis_date");
+
+                                        // Store info in SharedPreferences
+                                        SharedPreferences userInfo = getPreferences(Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor edit = userInfo.edit();
+                                        edit.putInt("uid", uid);
+                                        edit.putString("username", username.trim());
+                                        edit.putString("email", email.trim());
+                                        edit.putString("location", location.trim());
+                                        edit.putString("regis_date", regis_date.trim());
+                                        edit.apply();
+
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                        return;
+                                    } else {
+                                        String errorMsg = jsonObject.getString("error");
+                                        Toast.makeText(getApplicationContext(), errorMsg,
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    Log.d("JSON Error", e.toString());
+                                }
                                 Log.d("response: ",response);
                             }
                         }, new Response.ErrorListener() {
@@ -61,16 +103,21 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     protected Map<String, String> getParams() throws AuthFailureError {
                         Map<String, String> params = new HashMap<>();
-                        params.put("username", mUsername.getText().toString());
-                        params.put("password", mPassword.getText().toString());
+                        params.put("username", username);
+                        params.put("password", password);
                         return params;
                     }
                 };
                 // Add the request to the RequestQueue.
-                queue.add(stringRequest);
-
+                if(!username.equals("") && !password.equals("")) {
+                    queue.add(stringRequest);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter a username and password",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
 
         });
     }
 }
+
